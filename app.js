92bd8fe7355bae58097fe2fa37445c5495d386c9
bobur <bobur@gmail.com>
2025-01-22 10:25:51 +0500

    ok

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// for-1: k va n butun sonlari berilgan (n > 0). k sonini n marta chiqaruvchi programma tuzilsin.
+// // for-1: k va n butun sonlari berilgan (n > 0). k sonini n marta chiqaruvchi programma tuzilsin.
 
 let k = +prompt("k soniga qiymat kiriting:");
 let n = +prompt("n soniga qiymat kiriting:");
@@ -7,7 +7,7 @@ for (let i = 0; i < n; i++) {
     console.log(k);
 }
 
-// for-2 : Bir kg konfetning narxi berilgan (haqiqiy son). 0.1, 0.2, …, 0.9, 1 kg konfetni narxini chiqaruvchi programma tuzilsin.
+// // for-2 : Bir kg konfetning narxi berilgan (haqiqiy son). 0.1, 0.2, …, 0.9, 1 kg konfetni narxini chiqaruvchi programma tuzilsin.
 
 let price_candy = +prompt("1 kg konfet narxini kiriting:");
 
@@ -16,22 +16,22 @@ for (let i = 0.1; i <= 1; i+=0.1) {
     console.log(`${i}kg konfet narxi:`, (price_candy*i).toFixed(2));
 }
 
-// for-3:  n butun soni berilgan (n > 0). Quyidagi yig'indini hisoblovchi programma tuzilsin. 
-// S = n^2 +(n+1)^2+(n+2)^2 + ... +(2*n)^2
+// // for-3:  n butun soni berilgan (n > 0). Quyidagi yig'indini hisoblovchi programma tuzilsin. 
+// // S = n^2 +(n+1)^2+(n+2)^2 + ... +(2*n)^2
 
 
 let n_ = +prompt("n soniga qiymat bering:");
 let S = 0;
-for (let i = n_; i<=(2*n_)**2; i++) {
-    S+=i**2;
+for (let i = n_; i<=n_; i++) {
+    S+=(n_+i)**2;
 }
 console.log(`Natija: ${S};`);
 
-// for-4:  N butun soni berilgan. Quyidagi yig'indini chiqaruvchi programma tuzilsin. 
-// 1^N+2^(N-1) + ...+ N^1
-// Input: N = 5;
-// 1 ^ 5 + 2 ^ 4 + 3 ^ 3 + 4 ^ 2 + 5 ^ 1
-// Output: 65
+// // for-4:  N butun soni berilgan. Quyidagi yig'indini chiqaruvchi programma tuzilsin. 
+// // 1^N+2^(N-1) + ...+ N^1
+// // Input: N = 5;
+// // 1 ^ 5 + 2 ^ 4 + 3 ^ 3 + 4 ^ 2 + 5 ^ 1
+// // Output: 65
 
 let N = +prompt("n soniga qiymat bering:");
 let S_ = 0;
@@ -41,7 +41,7 @@ for (i=1; i <= 5; i++) {
 }
 console.log(`Natija: ${S_};`);
 
-// for-5: A va B butun soni berilgan (A < B). A va B sonlari orasidagi barcha butun sonlarni chiqaruvchi programma tuzilsin. Bunda har bir son o'zining qiymaticha chiqarilsin. Ya'ni 3 soni 3 marta chiqariladi.
+// // for-5: A va B butun soni berilgan (A < B). A va B sonlari orasidagi barcha butun sonlarni chiqaruvchi programma tuzilsin. Bunda har bir son o'zining qiymaticha chiqarilsin. Ya'ni 3 soni 3 marta chiqariladi.
 
 let A = +prompt("a soniga qiymat bering:");
 let B = +prompt("b soniga qiymat bering,(a sonidan katta qiymat kiriting):");
@@ -52,7 +52,7 @@ for (i = A; i <= B; i++) {
     }
 }
 
-// for-6: Sonning bo’luvchilarini topish;
+// // for-6: Sonning bo’luvchilarini topish;
 
 let son = +prompt("son kiriting:");
 
@@ -63,7 +63,7 @@ for (i=1; i<=son; i++) {
     }
 }
 
-// while-1:  N va K butun musbat sonlari berilgan. Faqat ayirish va qo'shish amallarini ishlatib N sonini K soniga bo'lgandagi qoldiq va butun qismini aniqlovchi programma tuzilsin.
+// // while-1:  N va K butun musbat sonlari berilgan. Faqat ayirish va qo'shish amallarini ishlatib N sonini K soniga bo'lgandagi qoldiq va butun qismini aniqlovchi programma tuzilsin.
 
 let D = +prompt("N soniga qiymat bering:");
 let H = +prompt("K soniga qiymat bering:");
@@ -80,11 +80,11 @@ for (let i = D; i>0; i+=0) {
     }
     i-=H;
 }
-console.log(`butun:${kop_d}; qoldiq: ${qol_h};`);
+// console.log(`butun:${kop_d}; qoldiq: ${qol_h};`);
 
-// while-2: 2 sonining qandaydir darajasini bildiruvchi n butun soni berilgan (n > 0). n = 2^k ni aniqlovchi programma tuzilsin.
-// Input: 128
-// Output: 7
+// // while-2: 2 sonining qandaydir darajasini bildiruvchi n butun soni berilgan (n > 0). n = 2^k ni aniqlovchi programma tuzilsin.
+// // Input: 128
+// // Output: 7
 
 let two_del = +prompt("2ning darajasi bolgan son kiriting:");
 let sum_two_del = 0;
@@ -98,7 +98,7 @@ for (let i = two_del; i!=1; i/=2) {
 }
 console.log(sum_two_del);
 
-// for-7:  a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b ni ham) chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi programma tuzilsin. (a va b xam chiqarilsin);
+// // for-7:  a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b ni ham) chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi programma tuzilsin. (a va b xam chiqarilsin);
 
 let e = +prompt("a soniga qiymat bering:");
 let y = +prompt("b soniga qiymat kiriting:");
@@ -111,7 +111,7 @@ for (let i=e; i<=y; i++) {
 console.log(sum_a_b,"ta son mavjud");
 
 
-// for-8 :  a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b dan tashqari) kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi progma tuzilsin;
+// // for-8 :  a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b dan tashqari) kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi progma tuzilsin;
 
 let e_ = +prompt("a soniga qiymat bering:");
 let y_ = +prompt("b soniga qiymat kiriting:");
